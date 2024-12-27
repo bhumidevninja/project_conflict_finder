0b9332d614eab1532ada63800f7935c9bed00d49
Bhumi Shah <bhumidevninja@gmail.com>
2024-12-27 16:59:06 +0530

    change setting file

--- project_conflict_finder/settings.py
+++ project_conflict_finder/settings.py
@@ -10,12 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 import os
-from dotenv import load_dotenv
 from pathlib import Path
 from datetime import timedelta
 
-# Load environment variables from .env file
-load_dotenv()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = "django-insecure-3pmdmgzqgro#x15)f%=_vy8xd=oi=d*_z!o-=qzo)9*$+_p50+"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', False)
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
